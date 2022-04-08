992198edebe41bb1b4e8d53fee2603de2a2f6d7f
GitHub Action <action@github.com>
2022-04-08 19:50:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/doctorjei/newplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/doctorjei/newplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.doctorjei.newplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.doctorjei.newplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/doctorjei/newplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/doctorjei/newplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.doctorjei.newplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.doctorjei.newplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
